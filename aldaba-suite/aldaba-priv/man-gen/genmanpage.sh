874bf05aaef66b84a8be48ad5e9615a3bcd45094
luis <luis@be774177-bb8f-0410-bfec-cc6af0dad470>
2010-09-08 11:11:04 +0000

    Modified man generation script so HTML and TXT versions are also generated

--- aldaba-suite/aldaba-priv/man-gen/genmanpage.sh
+++ aldaba-suite/aldaba-priv/man-gen/genmanpage.sh
@@ -22,6 +22,8 @@ echo "[+] Done!"
 echo "[+] Generating aldaba man page from aldaba-man.xml"
 #collateindex.pl -N -i idx -o genindex.sgm
 xsltproc --stringparam doc.class man --xinclude --output ../../docs/aldaba.8 xsl/man.nroff.xsl aldabamanhtml.xml
+man2html ../../docs/aldaba.8 > ../../docs/aldaba_man.html
+man  ../../docs/aldaba.8 > ../../docs/aldaba_man.txt
 echo "[+] Done!"
 
 
@@ -29,6 +31,8 @@ echo "[+] Done!"
 echo "[+] Generating aldabad man page from aldabad-man.xml"
 #collateindex.pl -N -i idx -o genindex.sgm
 xsltproc --stringparam doc.class man --xinclude --output ../../docs/aldaba.8 xsl/man.nroff.xsl aldabadmanhtml.xml
+man2html ../../docs/aldabad.8 > ../../docs/aldabad_man.html 
+man  ../../docs/aldabad.8 > ../../docs/aldabad_man.txt
 echo "[+] Done!"
 
 
